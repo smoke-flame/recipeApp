dc34993249cf7e4c0d97b31e77dd59bb1192218d
Serhii Siedniev <serhii.siedniev@drumncode.com>
2023-01-23 15:56:27 +0200

    add absolute path, move urls to env, add d.ts declarations

--- src/components/SearchModal/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const SearchModal = () => {
-  return <Text>SearchModal</Text>;
-};
-
-export default SearchModal;
---
+++ src/types/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module 'aws-amplify-react-native';
+
+declare module '@env' {
+  export const COOK_API_HOST: string;
+  export const COOK_API_KEY: string;
+  export const COOK_API_URL: string;
+}
